101fb90cb6efafad858d32daecaf96882c77ea00
koushikshetty94 <koushikshetty94@gmail.com>
2020-06-09 10:45:15 +0530

    handleclick

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={() => this.props.onClick()}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -21,15 +19,10 @@ class Board extends React.Component {
             squares: Array(9).fill(null)
         }
     }
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
-        if(i%2 === 0){
-            squares[i] = 'X';
-        }else{
-            squares[i] = 'o';
-        }
-
-        this.setState({squares: squares});
+        squares[i] = 'X';
+        this.setState({ squares: squares });
     }
     renderSquare(i) {
         return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
