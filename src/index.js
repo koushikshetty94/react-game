34820129407ba72f574efa70f15029908cb03e95
koushikshetty94 <koushikshetty94@gmail.com>
2020-06-09 11:14:54 +0530

    game complete

--- src/index.js
+++ src/index.js
@@ -30,12 +30,18 @@ class Board extends React.Component {
     }
     renderSquare(i) {
         return <Square value={this.state.squares[i]}
-                       onClick={() => this.handleClick(i)}
-                />;
+            onClick={() => this.handleClick(i)}
+        />;
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o ');
+        var winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner){
+            status = winner + ' is the winner'
+        }else{
+            status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o ');
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -75,6 +81,25 @@ class Game extends React.Component {
     }
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 // ========================================
 
 ReactDOM.render(
